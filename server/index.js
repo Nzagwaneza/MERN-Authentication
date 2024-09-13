d8146990b4350468378d49d59e4d030d58e63d49
Nzagwaneza <anzagwaneza@gmail.com>
2024-09-14 00:23:49 +0200

    Added  middleware and function to handle errors

--- server/index.js
+++ server/index.js
@@ -25,3 +25,13 @@ app.get("/", (req, res) => {
 
 app.use("/server/user", userRoutes);
 app.use("/server/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
