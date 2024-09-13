e28be18b764ed7a4c22cc9c393075cb6d4d2854f
Nzagwaneza <anzagwaneza@gmail.com>
2024-09-13 10:44:32 +0200

    Added Routes and Created Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Home from "./pages/Home";
 
 const App = () => {
-  return <h1 className="text-red-400">App</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
