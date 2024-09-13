6441791a085b42cdda98d8815bc999d0e084c6fe
Nzagwaneza <anzagwaneza@gmail.com>
2024-09-13 23:53:06 +0200

    Created sign up route

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 
 const app = express();
@@ -12,6 +13,8 @@ mongoose
   })
   .catch((error) => console.log(err));
 
+app.use(express.json()); //used because by default wwe can not send json to our app.
+
 app.listen(3000, () => {
   console.log("Server listening on port 3000!");
 });
@@ -21,3 +24,4 @@ app.get("/", (req, res) => {
 });
 
 app.use("/server/user", userRoutes);
+app.use("/server/auth", authRoutes);
