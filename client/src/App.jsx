0aacdcb0482a30ba90ff9a5aef282a08398233ab
Nzagwaneza <anzagwaneza@gmail.com>
2024-09-13 11:22:43 +0200

    Created header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Header from "./components/Header";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import About from "./pages/About";
@@ -10,6 +11,7 @@ import Home from "./pages/Home";
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
