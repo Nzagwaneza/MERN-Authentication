5089ad452dc3ae026b59fa35fcc414a89e1ec645
Nzagwaneza <anzagwaneza@gmail.com>
2024-10-07 08:17:23 +0200

    Created update user api route

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
-import cors from "cors";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -16,6 +16,7 @@ mongoose
   .catch((error) => console.log(error));
 
 app.use(express.json()); //used because by default wwe can not send json to our app.
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000!");
