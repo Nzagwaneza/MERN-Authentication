3c7a0a2d5b20bb70838a781474c4f8d9ef8a5a6d
Nzagwaneza <anzagwaneza@gmail.com>
2024-10-08 09:27:56 +0200

    Added  Sign out functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -15,6 +15,7 @@ import {
   deleteUserStart,
   deleteUserFailure,
   deleteUserSuccess,
+  signOut,
 } from "../redux/user/userSlice";
 import { errorHandler } from "../../../server/utils/error";
 
@@ -108,6 +109,15 @@ const Profile = () => {
     }
   };
 
+  const handleSignOut = async () => {
+    try {
+      await fetch("/server/auth/signout");
+      dispatch(signOut());
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg  mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -185,7 +195,9 @@ const Profile = () => {
         >
           Delete Account
         </span>
-        <span className="text-red-700 cursor-pointer">Sign out</span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">
+          Sign out
+        </span>
       </div>
       <p className="text-red-700 mt-5 ">{error && "Something went wrong!"}</p>
       <p className="text-green-600 mt-5 ">
